e6161d24e2081aa6c0a39d2e1401b1b87ca9620c
MarshandaHelmalia <146051076+MarshandaHelmalia@users.noreply.github.com>
2023-12-06 19:48:31 +0700

    Add files via upload

---
+++ hasil.py
@@ -0,0 +1,22 @@
+from bangundatar import *
+from operasiperhitungan import *
+
+#bangun datar
+persegi(5)
+persegi_panjang(7,12)
+segitiga_sama_sisi(6,6)
+jajar_genjang(3,10,4)
+lingkaran(18)
+belah_ketupat(10,8,6)
+layang_layang(3,2,4,4)
+
+#operasi perhitungan
+tambah(5,6)
+kurang(10,8)
+kali(5,8)
+bagi(20,2)
+pangkat(4,8)
+log(6)
+sin(90)
+cos(90)
+tan(45)
